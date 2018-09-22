ccc23322471bfc6afb47e6d159ec0486a208e3be
Chris Parnin <chris.parnin@gmail.com>
2018-09-22 00:05:55 -0400

    Migration to VS2017.

--- attachables/AttachablesPackage.cs
+++ attachables/AttachablesPackage.cs
@@ -14,6 +14,24 @@ using EnvDTE80;
 
 namespace ninlabs.attachables
 {
+    [Guid("2AAE0C73-D8A3-49C0-8070-630DC0F2BE16")]
+    public abstract class WindowKinds
+    {
+        public const string vsWindowKindKindStartPage = "{387CB18D-6153-4156-9257-9AC3F9207BBE}";
+        public const string vsWindowKindCommunityWindow = "{96DB1F3B-0E7A-4406-B73E-C6F0A2C67B97}";
+        public const string vsWindowKindDeviceExplorer = "{B65E9355-A4C7-4855-96BB-1D3EC8514E8F}";
+        public const string vsWindowKindBookmarks = "{A0C5197D-0AC7-4B63-97CD-8872A789D233}";
+        public const string vsWindowKindApplicationBrowser = "{399832EA-70A8-4AE7-9B99-3C0850DAD152}";
+        public const string vsWindowKindFavorites = "{57DC5D59-11C2-4955-A7B4-D7699D677E93}";
+        public const string vsWindowKindErrorList = "{D78612C7-9962-4B83-95D9-268046DAD23A}";
+        public const string vsWindowKindHelpSearch = "{46C87F81-5A06-43A8-9E25-85D33BAC49F8}";
+        public const string vsWindowKindHelpIndex = "{73F6DD58-437E-11D3-B88E-00C04F79F802}";
+        public const string vsWindowKindHelpContents = "{4A791147-19E4-11D3-B86B-00C04F79F802}";
+        public const string vsWindowKindCallBrowser = "{5415EA3A-D813-4948-B51E-562082CE0887}";
+        public const string vsWindowKindCodeDefinition = "{588470CC-84F8-4A57-9AC4-86BCA0625FF4}";
+        public const string vsWindowKindImmediate = "{28836128-FC2C-11D2-A433-00C04F72D18A}";
+    }
+
     /// <summary>
     /// This is the class that implements the package exposed by this assembly.
     ///
@@ -134,7 +152,7 @@ namespace ninlabs.attachables
 
         private void CancelErrorItem(object sender, EventArgs e)
         {
-            EnvDTE.Window window = this.dte.Windows.Item(EnvDTE80.WindowKinds.vsWindowKindErrorList);
+            EnvDTE.Window window = this.dte.Windows.Item(WindowKinds.vsWindowKindErrorList);
             ErrorList myErrorList = (EnvDTE80.ErrorList)window.Object;
             object[] objer = (object[])myErrorList.SelectedItems;
             foreach (object item in objer)
@@ -161,8 +179,8 @@ namespace ninlabs.attachables
 
         private void MarkErrorItemDone(object sender, EventArgs e)
         {
-            EnvDTE.Window window = this.dte.Windows.Item(EnvDTE80.WindowKinds.vsWindowKindErrorList);
-            ErrorList myErrorList = (EnvDTE80.ErrorList)window.Object;
+            EnvDTE.Window window = this.dte.Windows.Item(WindowKinds.vsWindowKindErrorList);
+            ErrorList myErrorList = (ErrorList)window.Object;
             object[] objer = (object[])myErrorList.SelectedItems;
             foreach (object item in objer)
             {
@@ -187,8 +205,8 @@ namespace ninlabs.attachables
 
         private void SnoozeErrorItem(object sender, EventArgs e)
         {
-            EnvDTE.Window window = this.dte.Windows.Item(EnvDTE80.WindowKinds.vsWindowKindErrorList);
-            ErrorList myErrorList = (EnvDTE80.ErrorList)window.Object;
+            EnvDTE.Window window = this.dte.Windows.Item(WindowKinds.vsWindowKindErrorList);
+            ErrorList myErrorList = (ErrorList)window.Object;
             object[] objer = (object[])myErrorList.SelectedItems;
             foreach (object item in objer)
             {
@@ -279,7 +297,7 @@ namespace ninlabs.attachables
         {
             OleMenuCommand menuItem = sender as OleMenuCommand;
             
-            EnvDTE.Window window = this.dte.Windows.Item(EnvDTE80.WindowKinds.vsWindowKindErrorList);
+            EnvDTE.Window window = this.dte.Windows.Item(WindowKinds.vsWindowKindErrorList);
             ErrorList myErrorList = (EnvDTE80.ErrorList)window.Object;
 
             // If there is any error selected, enable menu command and return.
